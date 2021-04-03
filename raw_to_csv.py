dabe6d94d3b9ac31e87a19f47fb09225f88031cf
Adei <49227821+adeiarias@users.noreply.github.com>
2021-04-03 14:15:42 +0200

    Update raw_to_csv.py

--- raw_to_csv.py
+++ raw_to_csv.py
@@ -4,5 +4,6 @@ raw_file = open(sys.argv[1], 'r', encoding="cp1252")
 raw_data_lines = raw_file.readlines()
 with open(sys.argv[2], 'w') as f:
     f.write("text,class\n")
-    for val in enumerate(raw_data_lines):
-        f.write(val.strip() + ',?\n')
+    for i,val in enumerate(raw_data_lines):
+        f.write(str(i) + ',' + str(val).strip().replace(',',' ') + ',?\n')
+f.close()
