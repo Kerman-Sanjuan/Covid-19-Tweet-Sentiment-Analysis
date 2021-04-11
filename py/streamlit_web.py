a8d15e94603817e48ba9e744ddc303ccad83e184
adeiarias <adeiariasupv@gmail.com>
2021-04-11 13:30:18 +0200

    Everything is ok

--- py/streamlit_web.py
+++ py/streamlit_web.py
@@ -133,11 +133,11 @@ def initialize_gui():
 
 st.title("COVID-19 Tweet Sentiment Analisys")
 #st.header("What type of upload method would you like to use?")
-df = pd.read_csv("../csv/headers.csv").drop('Sentiment', axis=1)
+df = pd.read_csv("csv/headers.csv").drop('Sentiment', axis=1)
 #initialize models(Logistic Regression and Neural Network)
-LR = pickle.load(open("../models/logistic_regression.pk", "rb"))
-NN = load_model('../models/NN.h5')
-vectorizer = pickle.load(open("../models/vectorizer.pk", "rb"))
+LR = pickle.load(open("models/logistic_regression.pk", "rb"))
+NN = load_model('models/NN.h5')
+vectorizer = pickle.load(open("models/vectorizer.pk", "rb"))
 initialize_gui()
 
 
