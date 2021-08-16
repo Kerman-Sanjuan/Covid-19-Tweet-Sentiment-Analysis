62df5f9e2bbe2fade13c60c4fd3491a1233c6ca6
Kerman-Sanjuan <kermansanjuanmalax@gmail.com>
2021-08-16 20:02:44 +0200

    Prueba

--- py/streamlit_web.py
+++ py/streamlit_web.py
@@ -25,95 +25,95 @@ global input_text
 
 
 def fix_input_text(text):
-	if str(input_text):
-		raw_data_lines = text.split("\n")
-		s = ""
-		for i in raw_data_lines:
-			if i != "\n":
-				s = s + " " + str(i)
-		return s.replace("\n"," ")
+    if str(input_text):
+        raw_data_lines = text.split("\n")
+        s = ""
+        for i in raw_data_lines:
+            if i != "\n":
+                s = s + " " + str(i)
+                return s.replace("\n"," ")
 
 
 def predict(pred_df):
-	st.subheader("RESULTS")
-	if classifier_type == "LOGISTIC REGRESSION":
-		y = LR.predict(pred_df)
-		if y[0] == 0:
-			st.error("Sentiment: NEGATIVE")
-		elif y[0] == 1:
-			st.warning("Sentiment: NEUTRAL")
-		elif y[0] == 2:
-			st.success("Sentiment: POSITIVE")
-	else: #NEURAL NETWORK
-		y = NN.predict(pred_df)
-		for i in y:
-			st.error("PERCENTAGE SENTIMENT: NEGATIVE -> %" + str(i[0]*100))
-			st.warning("PERCENTAGE SENTIMENT: NEUTRAL -> %" + str(i[1]*100))
-			st.success("PERCENTAGE SENTIMENT: POSITIVE -> %" + str(i[2]*100)) 
+    st.subheader("RESULTS")
+    if classifier_type == "LOGISTIC REGRESSION":
+        y = LR.predict(pred_df)
+        if y[0] == 0:
+            st.error("Sentiment: NEGATIVE")
+        elif y[0] == 1:
+            st.warning("Sentiment: NEUTRAL")
+        elif y[0] == 2:
+            st.success("Sentiment: POSITIVE")
+        else: #NEURAL NETWORK
+            y = NN.predict(pred_df)
+            for i in y:
+                st.error("PERCENTAGE SENTIMENT: NEGATIVE -> %" + str(i[0]*100))
+                st.warning("PERCENTAGE SENTIMENT: NEUTRAL -> %" + str(i[1]*100))
+                st.success("PERCENTAGE SENTIMENT: POSITIVE -> %" + str(i[2]*100)) 
 
-		st.subheader("PREDICTED SENTIMENT")
-		v = np.argmax(y[0])
-		if v == 0:
-			st.error("Sentiment: NEGATIVE")
-		elif v == 1:
-			st.warning("Sentiment: NEUTRAL")
-		elif v == 2:
-			st.success("Sentiment: POSITIVE")
-	
+                st.subheader("PREDICTED SENTIMENT")
+                v = np.argmax(y[0])
+                if v == 0:
+                    st.error("Sentiment: NEGATIVE")
+                elif v == 1:
+                    st.warning("Sentiment: NEUTRAL")
+                elif v == 2:
+                    st.success("Sentiment: POSITIVE")
+
 def print_prediction_legend():
-	st.header("PREDICTION LEGEND:")
-	st.success("GREEN IF RESULT EQUALS POSITIVE")
-	st.warning("YELLOW IF RESULT EQUALS NEUTRAL")
-	st.error("RED IF RESULT EQUALS NEGATIVE")	   
+    st.header("PREDICTION LEGEND:")
+    st.success("GREEN IF RESULT EQUALS POSITIVE")
+    st.warning("YELLOW IF RESULT EQUALS NEUTRAL")
+    st.error("RED IF RESULT EQUALS NEGATIVE")	   
 
 def print_preprocess_steps():
-	if not input_text:
-		st.warning("NO TWEET HAS BEEN WRITTEN")
-	else:
-		change = input_text
-		st.header("PREPROCESS STEPS")
+    if not input_text:
+        st.warning("NO TWEET HAS BEEN WRITTEN")
+    else:
+        change = input_text
+        st.header("PREPROCESS STEPS")
 
-		st.subheader("URL REMOVAL") 	
-		text = remove_urls(change)
-		st.write(text)
-		time.sleep(1.5)
+        st.subheader("URL REMOVAL") 	
+        text = remove_urls(change)
+        st.write(text)
+        time.sleep(1.5)
 
-		st.subheader("TEXT TO LOWERCASE")
-		text = text.lower()
-		st.write(text)
-		time.sleep(1.5)
+        st.subheader("TEXT TO LOWERCASE")
+        text = text.lower()
+        st.write(text)
+        time.sleep(1.5)
 
-		st.subheader("REMOVE STOPWORDS")
-		text = remove_stop_words(text)
-		st.write(text)
-		time.sleep(1.5)
+        st.subheader("REMOVE STOPWORDS")
+        text = remove_stop_words(text)
+        st.write(text)
+        time.sleep(1.5)
 
-		st.subheader("REMOVE PUNCTUATION")
-		text = remove_punctuation(text)
-		st.write(text)
-		time.sleep(1.5)
+        st.subheader("REMOVE PUNCTUATION")
+        text = remove_punctuation(text)
+        st.write(text)
+        time.sleep(1.5)
 
-		st.subheader("REMOVE EVERYTHING EXCEPT ALPHABET CHARACTERS")
-		text = re.sub('[^A-Za-z ]+', '', text)
-		st.write(text)
-		time.sleep(1.5)
+        st.subheader("REMOVE EVERYTHING EXCEPT ALPHABET CHARACTERS")
+        text = re.sub('[^A-Za-z ]+', '', text)
+        st.write(text)
+        time.sleep(1.5)
 
-		st.subheader("LEMMATIZE WORDS")
-		text = lemmatize_words(text)
-		st.write(text)
-		time.sleep(1.5)
+        st.subheader("LEMMATIZE WORDS")
+        text = lemmatize_words(text)
+        st.write(text)
+        time.sleep(1.5)
 
-		st.subheader("CORRECT SPELLING")
-		text = correct_spellings(text)
-		text = text.replace("ovid","covid")
-		st.write(text)
-		time.sleep(1.5)
+        st.subheader("CORRECT SPELLING")
+        text = correct_spellings(text)
+        text = text.replace("ovid","covid")
+        st.write(text)
+        time.sleep(1.5)
 
-		st.header("ORIGINAL TEXT")
-		st.write(input_text)
-		
-		st.header("AFTER PREPROCESS")
-		st.write(text)
+        st.header("ORIGINAL TEXT")
+        st.write(input_text)
+
+        st.header("AFTER PREPROCESS")
+        st.write(text)
 
 
 def check_raw_text(raw_text):
@@ -153,14 +153,14 @@ input_text = fix_input_text(input_text)
 
 
 if st.button("Make prediction"):
-	if not input_text:  # if text is empty
-		st.warning("Empty Text")
-	else:
-		st.success("Text processed correctly")
-		check_raw_text(input_text)
+    if not input_text:  # if text is empty
+        st.warning("Empty Text")
+    else:
+        st.success("Text processed correctly")
+        check_raw_text(input_text)
 
 if checkbox:
     print_preprocess_steps()
 
 if checkbox_legend:
-	print_prediction_legend()
+    print_prediction_legend()
